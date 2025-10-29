147eb3d737937f56b11dea5c566820b190c24aed
franciscogrimes <fgsieg1831@gmail.com>
2025-10-29 13:02:18 -0300

    fix: fix name to directory pages

--- src/components/routes/route.jsx
+++ src/components/routes/route.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "@/Layout";
-import Login from "@/Pages/Login";
-import Signup from "@/Pages/Signup";
-import Dashboard from "@/Pages/Dashboard";
-import Clientes from "@/Pages/Clientes";
-import Produtos from "@/Pages/Produtos";
-import Pedidos from "@/Pages/Pedidos";
+import Login from "@/pages/Login.jsx";
+import Signup from "@/pages/Signup";
+import Dashboard from "@/pages/Dashboard";
+import Clientes from "@/pages/Clientes";
+import Produtos from "@/pages/Produtos";
+import Pedidos from "@/pages/Pedidos";
 
 export const router = createBrowserRouter([
   {
